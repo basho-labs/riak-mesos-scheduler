39b1b05e40aeb1958ae01f69f2f8f69878a6007d
Nick Marino <nmarino@basho.com>
2016-01-13 15:25:50 -0500

    Add unit test module for HTTP API

---
+++ test/riak_mesos_wm_test.erl
@@ -0,0 +1,27 @@
+-module(riak_mesos_wm_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+riak_mesos_wm_test_() ->
+    SetupFun = fun() ->
+                       application:ensure_all_started(riak_mesos_scheduler),
+                       ok = mesos_scheduler_data:reset_all_data()
+               end,
+    TeardownFun = fun(_) ->
+                          application:stop(riak_mesos_scheduler)
+                  end,
+    {foreach,
+     SetupFun,
+     TeardownFun,
+     [
+      fun add_delete_cluster/0
+     ]}.
+
+-define(C1, "wm-test-cluster1").
+
+add_delete_cluster() ->
+    {ok, Res1} = httpc:request(get, {url("cluster/" ++ ?C1), []}, [], []),
+    ?assertMatch({{"HTTP/1.1", 404, "Object Not Found"}, _, _}, Res1).
+
+url(Resource) ->
+    "http://localhost:9090/" ++ Resource.
