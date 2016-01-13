a21b43382292065f240f4c0fe31ea31bfc579cde
Nick Marino <nmarino@basho.com>
2016-01-13 15:49:23 -0500

    Add more test code to verify creation and deletion

--- test/riak_mesos_wm_test.erl
+++ test/riak_mesos_wm_test.erl
@@ -20,12 +20,25 @@ riak_mesos_wm_test_() ->
 -define(C1, "wm-test-cluster1").
 
 add_delete_cluster() ->
-    {ok, Res1} = httpc:request(get, {url("clusters/" ++ ?C1), []}, [], []),
+    GetRequest = {url("clusters/" ++ ?C1), []},
+    {ok, Res1} = httpc:request(get, GetRequest, [], []),
     ?assertMatch({{"HTTP/1.1", 404, "Object Not Found"}, _, _}, Res1),
 
     CreateRequest = {url("clusters/" ++ ?C1), [], "plain/text", ""},
     {ok, Res2} = httpc:request(put, CreateRequest, [], []),
-    ?assertMatch({{"HTTP/1.1", 200, "OK"}, _, _}, Res2).
+    ?assertMatch({{"HTTP/1.1", 200, "OK"}, _, _}, Res2),
+
+    {ok, Res3} = httpc:request(get, GetRequest, [], []),
+    ?assertMatch({{"HTTP/1.1", 200, "OK"}, _, _}, Res3),
+    {_, _, GetJSON} = Res3,
+    Cluster = mochijson2:decode(GetJSON),
+    ?assertMatch({struct, [{<<?C1>>, {struct, _}}]}, Cluster),
+
+    {ok, Res4} = httpc:request(delete, GetRequest, [], []),
+    ?assertMatch({{"HTTP/1.1", 200, "OK"}, _, _}, Res4),
+
+    {ok, Res5} = httpc:request(get, GetRequest, [], []),
+    ?assertMatch({{"HTTP/1.1", 404, "Object Not Found"}, _, _}, Res5).
 
 url(Resource) ->
     "http://localhost:9090/api/v1/" ++ Resource.
