62adc8ff594c0153082d98d3432ed0a64119ea70
Nick Marino <nmarino@basho.com>
2016-01-15 16:20:22 -0500

    Implement config-related API and add tests
    This implements all of the HTTP API functionality for getting and
    setting the riak.conf and advanced.config files.

--- test/riak_mesos_wm_test.erl
+++ test/riak_mesos_wm_test.erl
@@ -15,7 +15,8 @@ riak_mesos_wm_test_() ->
      TeardownFun,
      [
       fun add_delete_cluster/0,
-      fun list_clusters/0
+      fun list_clusters/0,
+      fun set_get_cluster_config/0
      ]}.
 
 -define(C1, "wm-test-cluster1").
@@ -58,6 +59,41 @@ list_clusters() ->
     {_, _, ListJSON2} = Res3,
     ClusterList2 = mochijson2:decode(ListJSON2),
     ?assertEqual({struct, [{<<"clusters">>, [<<?C1>>]}]}, ClusterList2).
+
+set_get_cluster_config() ->
+    RiakConfUrl = url("/clusters/" ++ ?C1 ++ "/riak.conf"),
+    AdvancedConfUrl = url("/clusters/" ++ ?C1 ++ "/advanced.config"),
+
+    GetRiakConfRequest = {RiakConfUrl, []},
+    GetAdvancedConfRequest = {AdvancedConfUrl, []},
+
+    verify_http_request(get, GetRiakConfRequest, 404),
+    verify_http_request(get, GetAdvancedConfRequest, 404),
+
+    CreateRequest = {url("clusters/" ++ ?C1), [], "plain/text", ""},
+    verify_http_request(put, CreateRequest, 200),
+
+    %% Getting config should succeed now, but actual config contents
+    %% are undefined (in practice will probably default to "").
+    verify_http_request(get, GetRiakConfRequest, 200),
+    verify_http_request(get, GetAdvancedConfRequest, 200),
+
+    TestConfig = "fake-test-config",
+    SetRiakConfRequest = {RiakConfUrl, [], "plain/text", TestConfig},
+    SetAdvancedConfRequest = {AdvancedConfUrl, [], "plain/text", TestConfig},
+
+    verify_http_request(put, SetRiakConfRequest, 200),
+    verify_http_request(put, SetAdvancedConfRequest, 200),
+
+    {_, _, CurrentRiakConfig} = verify_http_request(get, GetRiakConfRequest, 200),
+    {_, _, CurrentAdvancedConfig} = verify_http_request(get, GetAdvancedConfRequest, 200),
+    ?assertEqual(CurrentRiakConfig, TestConfig),
+    ?assertEqual(CurrentAdvancedConfig, TestConfig).
+
+verify_http_request(Method, Request, ExpectedCode) ->
+    {ok, Res} = httpc:request(Method, Request, [], []),
+    ?assertMatch({{"HTTP/1.1", ExpectedCode, _}, _, _}, Res),
+    Res.
 
 url(Resource) ->
     "http://localhost:9090/api/v1/" ++ Resource.
