9275c171942bc1071220239dbe0c415e9339e13f
Dmitry Platon <platon.dimka@gmail.com>
2016-01-28 17:36:14 +0300

    Reserved and unreserved resources added

--- src/riak_mesos_offer_helper.erl
+++ src/riak_mesos_offer_helper.erl
@@ -2,9 +2,20 @@
 
 -include_lib("erl_mesos/include/scheduler_protobuf.hrl").
 
+-include_lib("erl_mesos/include/utils.hrl").
+
 -export([new/1,
          get_offer_id_value/1,
-         get_persistence_ids/1]).
+         get_persistence_ids/1,
+         get_reserved_resources/1,
+         get_reserved_resources_cpus/1,
+         get_reserved_resources_mem/1,
+         get_reserved_resources_disk/1,
+         get_reserved_resources_ports/1,
+         get_unreserved_resources_cpus/1,
+         get_unreserved_resources_mem/1,
+         get_unreserved_resources_disk/1,
+         get_unreserved_resources_ports/1]).
 
 -record(offer_helper, {offer :: erl_mesos:'Offer'(),
                        offer_id_value :: string(),
@@ -23,15 +34,61 @@
 
 new(#'Offer'{id = #'OfferID'{value = OfferIdValue},
              resources = Resources} = Offer) ->
+    PersistenceIds = get_persistence_ids(Resources, []),
+    ReservedCpus = get_scalar_resource_value("cpus", true, Resources),
+    ReservedMem = get_scalar_resource_value("mem", true, Resources),
+    ReservedDisk = get_scalar_resource_value("disk", true, Resources),
+    ReservedPorts = get_ranges_resource_values("ports", true, Resources),
+    ReservedResources = resources(ReservedCpus, ReservedMem, ReservedDisk,
+                                  ReservedPorts),
+    UnreservedCpus = get_scalar_resource_value("cpus", false, Resources),
+    UnreservedMem = get_scalar_resource_value("mem", false, Resources),
+    UnreservedDisk = get_scalar_resource_value("disk", false, Resources),
+    UnreservedPorts = get_ranges_resource_values("ports", false, Resources),
+    UnreservedResources = resources(UnreservedCpus, UnreservedMem,
+                                    UnreservedDisk, UnreservedPorts),
     #offer_helper{offer = Offer,
                   offer_id_value = OfferIdValue,
-                  persistence_ids = get_persistence_ids(Resources, [])}.
+                  persistence_ids = PersistenceIds,
+                  reserved_resources = ReservedResources,
+                  unreserved_resources = UnreservedResources}.
 
 get_offer_id_value(#offer_helper{offer_id_value = OfferIdValue}) ->
     OfferIdValue.
 
 get_persistence_ids(#offer_helper{persistence_ids = PersistenceIds}) ->
     PersistenceIds.
+
+get_reserved_resources(#offer_helper{reserved_resources = ReservedResources}) ->
+    ReservedResources.
+
+get_reserved_resources_cpus(OfferHelper) ->
+    erl_mesos_utils:resources_cpus(get_reserved_resources(OfferHelper)).
+
+get_reserved_resources_mem(OfferHelper) ->
+    erl_mesos_utils:resources_mem(get_reserved_resources(OfferHelper)).
+
+get_reserved_resources_disk(OfferHelper) ->
+    erl_mesos_utils:resources_disk(get_reserved_resources(OfferHelper)).
+
+get_reserved_resources_ports(OfferHelper) ->
+    erl_mesos_utils:resources_ports(get_reserved_resources(OfferHelper)).
+
+get_unreserved_resources(#offer_helper{unreserved_resources =
+                                           UnreservedResources}) ->
+    UnreservedResources.
+
+get_unreserved_resources_cpus(OfferHelper) ->
+    erl_mesos_utils:resources_cpus(get_unreserved_resources(OfferHelper)).
+
+get_unreserved_resources_mem(OfferHelper) ->
+    erl_mesos_utils:resources_mem(get_unreserved_resources(OfferHelper)).
+
+get_unreserved_resources_disk(OfferHelper) ->
+    erl_mesos_utils:resources_disk(get_unreserved_resources(OfferHelper)).
+
+get_unreserved_resources_ports(OfferHelper) ->
+    erl_mesos_utils:resources_ports(get_unreserved_resources(OfferHelper)).
 
 %% ====================================================================
 %% Private
@@ -51,4 +108,68 @@ get_persistence_ids([_Resource | Resources], PersistenceIds) ->
 get_persistence_ids([], PersistenceIds) ->
     lists:reverse(PersistenceIds).
 
+get_scalar_resource_value(Name, WithReservation, Resources) ->
+    get_scalar_resource_value(Name, WithReservation, Resources, 0.0).
 
+get_scalar_resource_value(Name, true,
+                          [#'Resource'{name = Name,
+                                       type = 'SCALAR',
+                                       scalar = #'Value.Scalar'{value =
+                                                                  ScalarValue},
+                                       reservation = Reservation} |
+                           Resources], Value)
+  when Reservation =/= undefined  ->
+    get_scalar_resource_value(Name, true, Resources, Value + ScalarValue);
+get_scalar_resource_value(Name, false,
+                          [#'Resource'{name = Name,
+                                       type = 'SCALAR',
+                                       scalar = #'Value.Scalar'{value =
+                                                                  ScalarValue},
+                                       reservation = undefined} |
+                           Resources], Value) ->
+    get_scalar_resource_value(Name, false, Resources, Value + ScalarValue);
+get_scalar_resource_value(Name, WithReservation, [_Resource | Resources],
+                          Value) ->
+    get_scalar_resource_value(Name, WithReservation, Resources, Value);
+get_scalar_resource_value(_Name, _WithReservation, [], Value) ->
+    Value.
+
+get_ranges_resource_values(Name, WithReservation, Resources) ->
+    get_ranges_resource_values(Name, WithReservation, Resources, []).
+
+get_ranges_resource_values(Name, true,
+                           [#'Resource'{name = Name,
+                                        type = 'RANGES',
+                                        ranges = #'Value.Ranges'{range =
+                                                                     Ranges},
+                                        reservation = Reservation} |
+                           Resources], Values)
+  when Reservation =/= undefined  ->
+    Values1 = add_ranges_values(Ranges, Values),
+    get_ranges_resource_values(Name, true, Resources, Values1);
+get_ranges_resource_values(Name, false,
+                           [#'Resource'{name = Name,
+                                        type = 'RANGES',
+                                        ranges = #'Value.Ranges'{range =
+                                                                     Ranges},
+                                        reservation = undefined} |
+                            Resources], Values) ->
+    Values1 = add_ranges_values(Ranges, Values),
+    get_ranges_resource_values(Name, false, Resources, Values1);
+get_ranges_resource_values(Name, WithReservation, [_Resource | Resources],
+                           Values) ->
+    get_ranges_resource_values(Name, WithReservation, Resources, Values);
+get_ranges_resource_values(_Name, _WithReservation, [], Values) ->
+    Values.
+
+add_ranges_values(Ranges, Values) ->
+    lists:foldl(fun(#'Value.Range'{'begin' = Begin,
+                                   'end' = End}, Acc) ->
+                    Acc ++ lists:seq(Begin, End)
+                end, Values, Ranges).
+
+resources(Cpus, Mem, Disk, Ports) ->
+    #resources{cpus = Cpus,
+               mem = Mem,
+               disk = Disk,
+               ports = Ports}.
--- test/riak_mesos_offer_helper_SUITE.erl
+++ test/riak_mesos_offer_helper_SUITE.erl
@@ -12,25 +12,90 @@ all() ->
     [new].
 
 new(_Config) ->
-    Offer1 = offer("offer_1", []),
+    OfferResources1 = [],
+    Offer1 = offer("offer_1", OfferResources1),
     OfferHelper1 = riak_mesos_offer_helper:new(Offer1),
     "offer_1" = riak_mesos_offer_helper:get_offer_id_value(OfferHelper1),
     [] = riak_mesos_offer_helper:get_persistence_ids(OfferHelper1),
+    0.0 = riak_mesos_offer_helper:get_reserved_resources_cpus(OfferHelper1),
+    0.0 = riak_mesos_offer_helper:get_reserved_resources_mem(OfferHelper1),
+    0.0 = riak_mesos_offer_helper:get_reserved_resources_disk(OfferHelper1),
+    [] = riak_mesos_offer_helper:get_reserved_resources_ports(OfferHelper1),
 
-    Offer2 = offer("offer_2", resources_with_persistence_ids()),
+    OfferResources2 = cpus_resources_reservation() ++
+                      mem_resources_reservation() ++
+                      ports_resources_reservation() ++
+                      volume_resources_reservation() ++
+                      cpus_resources() ++
+                      mem_resources() ++
+                      ports_resources(),
+    Offer2 = offer("offer_2", OfferResources2),
     OfferHelper2 = riak_mesos_offer_helper:new(Offer2),
     "offer_2" = riak_mesos_offer_helper:get_offer_id_value(OfferHelper2),
     ["id_1", "id_2"] =
         riak_mesos_offer_helper:get_persistence_ids(OfferHelper2),
-
+    CpusReservation = 0.2 + 0.3 + 0.4,
+    CpusReservation =
+        riak_mesos_offer_helper:get_reserved_resources_cpus(OfferHelper2),
+    MemReservation = 256.0 + 512.0 + 1024.0,
+    MemReservation =
+        riak_mesos_offer_helper:get_reserved_resources_mem(OfferHelper2),
+    VolumReservation = 1024.0 + 2048.0,
+    VolumReservation =
+        riak_mesos_offer_helper:get_reserved_resources_disk(OfferHelper2),
+    PortsReservation = lists:seq(1, 6),
+    PortsReservation =
+        riak_mesos_offer_helper:get_reserved_resources_ports(OfferHelper2),
+    Cpus = 0.1 + 0.2 + 0.3,
+    Cpus = riak_mesos_offer_helper:get_unreserved_resources_cpus(OfferHelper2),
+    Mem = 128.0 + 256.0 + 512.0,
+    Mem = riak_mesos_offer_helper:get_unreserved_resources_mem(OfferHelper2),
+    Ports = lists:seq(7, 12),
+    Ports =
+        riak_mesos_offer_helper:get_unreserved_resources_ports(OfferHelper2),
     ok.
 
+cpus_resources_reservation() ->
+    [erl_mesos_utils:scalar_resource_reservation("cpus", 0.2, "*",
+                                                 "principal"),
+     erl_mesos_utils:scalar_resource_reservation("cpus", 0.3, "*",
+                                                 "principal"),
+     erl_mesos_utils:scalar_resource_reservation("cpus", 0.4, "*",
+                                                 "principal")].
 
-resources_with_persistence_ids() ->
-    [erl_mesos_utils:volume_resource_reservation(0.1, "id_1", "path_1", 'RW',
+mem_resources_reservation() ->
+    [erl_mesos_utils:scalar_resource_reservation("mem", 256.0, "*",
+                                                 "principal"),
+     erl_mesos_utils:scalar_resource_reservation("mem", 512.0, "*",
+                                                 "principal"),
+     erl_mesos_utils:scalar_resource_reservation("mem", 1024.0, "*",
+                                                 "principal")].
+
+ports_resources_reservation() ->
+    [erl_mesos_utils:ranges_resource_reservation("ports", [{1, 3}], "*",
+                                                 "principal"),
+     erl_mesos_utils:ranges_resource_reservation("ports", [{4, 6}], "*",
+                                                 "principal")].
+
+volume_resources_reservation() ->
+    [erl_mesos_utils:volume_resource_reservation(1024.0, "id_1", "path_1", 'RW',
                                                  "*", "principal"),
-     erl_mesos_utils:volume_resource_reservation(0.2, "id_2", "path_2", 'RW',
+     erl_mesos_utils:volume_resource_reservation(2048.0, "id_2", "path_2", 'RW',
                                                  "*", "principal")].
+
+cpus_resources() ->
+    [erl_mesos_utils:scalar_resource("cpus", 0.1),
+     erl_mesos_utils:scalar_resource("cpus", 0.2),
+     erl_mesos_utils:scalar_resource("cpus", 0.3)].
+
+mem_resources() ->
+    [erl_mesos_utils:scalar_resource("mem", 128.0),
+     erl_mesos_utils:scalar_resource("mem", 256.0),
+     erl_mesos_utils:scalar_resource("mem", 512.0)].
+
+ports_resources() ->
+    [erl_mesos_utils:ranges_resource("ports", [{7, 9}]),
+     erl_mesos_utils:ranges_resource("ports", [{10, 12}])].
 
 offer(OfferIdValue, Resources) ->
     #'Offer'{id = #'OfferID'{value = OfferIdValue},
