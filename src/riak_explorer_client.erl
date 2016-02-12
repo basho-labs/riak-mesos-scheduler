b1fb51205cc0e51629784a656c9e474c7f835d55
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-02-12 12:34:55 -0500

    temp commit

---
+++ src/riak_explorer_client.erl
@@ -0,0 +1,62 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015 Basho Technologies Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(riak_explorer_client).
+
+%% @doc Sends http request.
+%% @private
+-spec send_request(erl_mesos_scheduler:scheduler_info(), erl_mesos:'Call'()) ->
+    {ok, erl_mesos_http:client_ref()} |
+    {ok, non_neg_integer(), erl_mesos_http:headers(),
+     erl_mesos_http:client_ref()} |
+    {error, term()}.
+send_request(#scheduler_info{data_format = DataFormat,
+                             api_version = ApiVersion,
+                             master_host = MasterHost,
+                             request_options = RequestOptions}, Call) ->
+    ReqUrl = request_url(ApiVersion, MasterHost),
+    ReqHeaders = request_headers(DataFormat),
+    ReqBody = erl_mesos_data_format:encode(DataFormat, Call),
+    hackney:request(Method, Url, Headers, Body, Options).
+    erl_mesos_http:request(post, ReqUrl, ReqHeaders, ReqBody, RequestOptions).
+
+%% @doc Returns request url.
+%% @private
+-spec request_url(version(), binary()) -> binary().
+request_url(v1, MasterHost) ->
+    <<"http://", MasterHost/binary, ?V1_API_PATH>>.
+
+%% @doc Returns request headers.
+%% @private
+-spec request_headers(erl_mesos_data_format:data_format()) ->
+    erl_mesos_http:headers().
+request_headers(DataFormat) ->
+    ContentType = erl_mesos_data_format:content_type(DataFormat),
+    [{<<"Content-Type">>, ContentType},
+     {<<"Accept">>, ContentType},
+     {<<"Connection">>, <<"close">>}].
+
+
+%% @doc Sends http request.
+-spec request(atom(), binary(), headers(), binary(), options()) ->
+    {ok, client_ref()} | {ok, non_neg_integer(), headers(), client_ref()} |
+    {error, term()}.
+request(Method, Url, Headers, Body, Options) ->
+    
