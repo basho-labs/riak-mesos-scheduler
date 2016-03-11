c7e3137d69f31c2e75a0ad11cfe9293404dd2751
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-03-11 17:20:30 -0500

    putting files in correct places

--- rel/files/ermf-scheduler.sh
+++ rel/files/ermf-scheduler.sh
@@ -19,6 +19,24 @@ main() {
         export RIAK_MESOS_PORT=$PORT0
     fi
 
+    mv riak-*.tar.gz riak.tar.gz  &> /dev/null
+    mv riak_ts-*.tar.gz riak.tar.gz  &> /dev/null
+    mv riak_ee-*.tar.gz riak.tar.gz  &> /dev/null
+    mv riak_explorer-*.tar.gz riak_explorer.tar.gz
+    mv riak_mesos_executor-*.tar.gz riak_mesos_executor.tar.gz
+    rm -rf root
+    rm -rf riak_mesos_executor
+
+    if [ -z "$RIAK_MESOS_EXECUTOR_PKG"]; then
+        export RIAK_MESOS_EXECUTOR_PKG=riak_mesos_executor.tar.gz
+    fi
+    if [ -z "$RIAK_MESOS_EXPLORER_PKG"]; then
+        export RIAK_MESOS_EXPLORER_PKG=riak_explorer.tar.gz
+    fi
+    if [ -z "$RIAK_MESOS_RIAK_PKG"]; then
+        export RIAK_MESOS_RIAK_PKG=riak.tar.gz
+    fi
+
     echo "Starting riak_mesos_scheduler..."
     riak_mesos_scheduler/bin/riak_mesos_scheduler console -noinput
 }
