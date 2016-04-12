a45c8784bccc74626c0c8595db0b073a0c281691
sanmiguel <michael.coles@gmail.com>
2016-04-12 16:04:03 +0200

    oops this function got renamed

--- src/rms_wm_resource.erl
+++ src/rms_wm_resource.erl
@@ -328,7 +328,7 @@ add_node(ReqData) ->
 delete_node(ReqData) ->
     NodeKey = wrq:path_info(node_key, ReqData),
     Force = list_to_atom(wrq:get_qs_value("force", "false", ReqData)),
-    Response = build_response(rms_node_manager:delete_node(NodeKey, Force)),
+    Response = build_response(rms_node_manager:destroy_node(NodeKey, Force)),
     {true, wrq:append_to_response_body(mochijson2:encode(Response), ReqData)}.
 
 restart_node(RD) ->
